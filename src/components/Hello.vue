00ad4051470c1ab37f00af37e40cc19c0be36a8f
luoyi3 <luoyi3@beisen.com>
2017-09-10 21:16:29 +0800

    vue todo

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <input type="" name="" v-model = "newTodo" @keyup.enter = 'addNew'>
+    <ul>
+      <li v-for='(todo, index) in todos' :id="index" :class= "{'done':todo.done}" @click='toggleDone(todo)'>
+        <label>{{index + 1}}.{{todo.value}}</label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      title: 'this is a vue-todolist',
+      todos: [
+        // {value: '阅读一本关于前端开发的书', done: false},
+        // {value: '补充范例代码', done: true},
+        // {value: '写心得', done: false},
+        // {value: '写博客', done: false}
+      ],
+      newTodo: ''
+    }
+  },
+  methods: {
+    toggleDone: function (todo) {
+      todo.done = !todo.done
+    },
+    addNew: function () {
+      this.todos.push({value: this.newTodo, done: false})
+      this.newTodo = ''
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.done{
+  text-decoration: underline;
+}
+</style>
